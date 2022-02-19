277db5deac0a244fd950c81d9bca49bb49364dc8
VatsalshinAgla <d19dcs157@charusat.edu.in>
2022-02-19 13:15:58 +0530

    feature-date and time

---
+++ todoapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DateTime from './component/DateTime';
+
+function App() {
+  return (
+    <div className="App">
+     <DateTime/>
+    </div>
+  );
+}
+
+export default App;
