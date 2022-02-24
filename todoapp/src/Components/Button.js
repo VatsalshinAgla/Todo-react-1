13f117550b1ae761841b2ec8bbce30dad1e5a548
VatsalshinAgla <d19dcs157@charusat.edu.in>
2022-02-24 12:39:42 +0530

    feature:NewTaskForm

--- todoapp/src/Components/Button.js
+++ todoapp/src/Components/Button.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import NewTaskForm from './NewTaskForm';
 import './style/Button.css'
-function Button() {
-  return (
-    <div className='button'>
-            <button className='plus'>+</button>
-    </div>
+function Button(props) {
+  const [isAdding, setIsAdding] = useState(false);
+  
+  const forwardTaskToList = (task) =>{
+    props.getTask(task)
+
+  }
+  const startAddingHandler = () => {
+    setIsAdding(true)
+  }
+  const stopAddingHandler = () => {
+    setIsAdding(false)
+  }
+  return(
+   <div className='button'>
+    {!isAdding && <button onClick={startAddingHandler} className='plus' >+</button>}
+    {isAdding && <NewTaskForm onEscape = {stopAddingHandler} onAddTask={forwardTaskToList}/>}
+      
+  </div>
+  // return (
+  //   <div className='button'>
+  //           <button className='plus'>+</button>
+  //   </div>
   );
 }
 
